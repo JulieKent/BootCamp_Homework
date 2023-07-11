c8a693ec976fc1fddf9817361fddb03e195c5281
JulieKent <julie-kent@bigpond.com>
2023-07-11 11:36:46 +1000

    final PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,44 +1,79 @@
 import os
 import csv
-import collections
+from collections import defaultdict, Counter
 
 election_data_csv = os.path.join('Resources','election_data.csv')
-analysis_export = os.path.join('analysis', 'analysis.txt')
+analysis_export = os.path.join('analysis', 'analysis_export.txt')
+
+#lines 9, 14, 17-19, 22-31, sourced and adapted from (https://stackoverflow.com/questions/52012798/python3-dictionary-from-csv-file-to-count-frequency-of-words)
+columns = defaultdict(list)
 
 with open(election_data_csv, 'r') as csvfile:
 
-    #split the columns on comma
-    csvreader = csv.reader(csvfile, delimiter=',')
+    #read in csv to dictionary format
+    reader = csv.DictReader(csvfile)
 
-    #skip header row
-    header = next(csvfile,None)
+    #append csv to columns
+    for row in reader:
+        for (k,v) in row.items():
+            columns[k].append(v)
 
-    #define inital variable values
-    rowcount = 0
-    votecount = 0
+    #define target column for list 
+    candidate_list = columns['Candidate']
 
-    #define lists
-    cand = []
-    votes1 = []
-    votes2 = []
-    votes3 = []
-    #append unique values to list (https://stackoverflow.com/questions/24441606/how-to-create-a-list-in-python-with-the-unique-values-of-a-csv-file)
-    for row in csvreader:
-        if row[2] not in cand:
-            cand.append(row[2])
-        rowcount += 1
-    
-    #count votes per candidate
+    #create empty list 
+    candidate_list_clean = []
 
+    #append candidates to the empty list 
+    for cand in candidate_list:
+        candidate_list_clean.append(cand)
+
+    output = dict(Counter(candidate_list_clean)) 
+
+    #sum the total votes
+    total = sum(output.values())
+
+    #create empty list for results
+    result = []
+
+    #print results to terminal and append to list for export to txt file
     print("")
+    result.append("")
     print("Election Results\n")
+    result.append("Election Results\n")
     print("----------------------------\n")
-    print(f"Total Votes: {rowcount}\n")
+    result.append("----------------------------\n")
+    print(f"Total Votes: {total}\n")
+    result.append(f"Total Votes: {total}\n")
     print("----------------------------\n")
+    result.append("----------------------------\n")
 
-    for can in range(len(cand)):
-        print(f"{cand[can]}:\n")
+    #calculate percentage of votes per candicate (https://stackoverflow.com/questions/43084138/calculate-the-percentage-for-python-dictionary)
+    for c, t in output.items():
+        perc = round(t *100/total, 3)
+        print(f"{c}: {perc}% ({t})\n")
+        result.append(f"{c}: {perc}% ({t})\n")
+
+    #determin candidate with the most votes (https://www.geeksforgeeks.org/python-get-key-with-maximum-value-in-dictionary/)
+    winner = max(zip(output.values(), output.keys()))[1]
 
     print("----------------------------\n")
-    print(f"Winniner: \n")
+    result.append("----------------------------\n")
+    print(f"Winniner: {winner}\n")
+    result.append(f"Winniner: {winner}\n")
     print("----------------------------\n")
+    result.append("----------------------------\n")
+
+#open text file
+with open(analysis_export, 'w', newline='') as txtfile:
+    
+    #write each item in the results list to the text file
+    for item in result:
+        txtfile.write("%s\n" % item)
+    
+    #close the text file
+    txtfile.close
+
+
+
+
